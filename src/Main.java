c2df0c7e87f1ae03c3434f44d58b924c4f010e74
Federico Maiello <federicomaiello@hotmail.it>
2023-11-15 16:17:02 +0100

    add code

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,17 @@ package src;
 import java.io.NotActiveException;
 
 public class Main {
-    public static void main(String[] args) {
-        try{
-            int checkNumber = 10;
-            int number1 = 0;
-            int number2 = 10;
-
-            boolean result = checkRange(checkNumber, number1, number2);
-            if (result) {
-                System.out.println(checkNumber + " is in the range.");
-            } //else {
-//                System.out.println(checkNumber + " Isn't in the range.");
-//            }
-
-            } catch (IllegalArgumentException e){
-            System.out.println(e.getMessage());
-        }
-
+    public static void main(String[] args) throws Exception {
+        boolean isInRange = checkRange(11, 0, 10);
+        System.out.println("The number is in range: " + isInRange);
+        System.out.println("end!");
     }
-    public static boolean checkRange (int checkNumber, int number1, int number2){
-        if (checkNumber >= number1 && checkNumber <= number2){
-            return true;
+    public static boolean checkRange (int checkNumber, int number1, int number2) throws Exception {
+        if (checkNumber <= number1 || checkNumber >= number2){
+            throw new Exception("Error: Number isn't in the range!");
+
         } else {
-            throw new IllegalArgumentException("Error: Number isn't in the range!");
+            return true;
         }
     }
 }
